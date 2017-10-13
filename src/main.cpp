73e428ce269f1bd9b5430e81d92206f17508ade4
Igor Molina <canutito1@hotmail.com>
2017-10-13 07:21:51 +0200

    Small changes to src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -250,42 +250,40 @@ int main() {
                 vector<double> ptsx;
                 vector<double> ptsy;
 
-                double pos_x;
-                double pos_y;
-                double angle;
-                int path_size = previous_path_x.size();
+                double ref_x = car_x;
+                double ref_y = car_y;
+                double ref_yaw = deg2rad(car_yaw);
+                int prev_size = previous_path_x.size();
 
 
-                if(path_size < 2)
+                if(prev_size < 2)
                 {
-                  pos_x = car_x;
-                  pos_y = car_y;
-                  angle = deg2rad(car_yaw);
-                  double ref_x_prev = (car_x - cos(car_yaw));
-                  double ref_y_prev = (car_y - sin(car_yaw));
-
                   //use two points that make path tangent to previus path's end point
-                  ptsx.push_back(ref_x_prev);
-                  ptsx.push_back(pos_x);
+                  double prev_car_x = car_x - cos(car_yaw);
+                  double prev_car_y = car_y - sin(car_yaw);
 
-                  ptsy.push_back(ref_y_prev);
-                  ptsy.push_back(pos_y);
+                  ptsx.push_back(prev_car_x);
+                  ptsx.push_back(car_x);
+
+                  ptsy.push_back(prev_car_y);
+                  ptsy.push_back(car_y);
+
                 }
                 else
                 {
-                  pos_x = previous_path_x[path_size-1];
-                  pos_y = previous_path_y[path_size-1];
+                  ref_x = previous_path_x[prev_size-1];
+                  ref_y = previous_path_y[prev_size-1];
 
-                  double pos_x2 = previous_path_x[path_size-2];
-                  double pos_y2 = previous_path_y[path_size-2];
-                  angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+                  double ref_x_prev = previous_path_x[prev_size-2];
+                  double ref_y_prev = previous_path_y[prev_size-2];
+                  ref_yaw = atan2(ref_y-ref_y_prev,ref_x-ref_x_prev);
 
                   //use two points that make path tangent to previus path's end point
-                  ptsx.push_back(pos_x2);
-                  ptsx.push_back(pos_x);
+                  ptsx.push_back(ref_x_prev);
+                  ptsx.push_back(ref_x);
 
-                  ptsy.push_back(pos_y2);
-                  ptsy.push_back(pos_y);
+                  ptsy.push_back(ref_y_prev);
+                  ptsy.push_back(ref_y);
                 }
                 //In Frenet add evenly 30m spaced points ahead of the starting reference
                 vector<double> next_mp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
@@ -305,11 +303,11 @@ int main() {
                 for(int i = 0; i < ptsx.size(); i++)
                 {    
                    //shift car reference angle to 0 degrees
-                   double shift_x = (ptsx[i]-pos_x);
-                   double shift_y = (ptsy[i]-pos_y);
+                   double shift_x = (ptsx[i]-ref_x);
+                   double shift_y = (ptsy[i]-ref_y);
 
-                   ptsx[i] = ((shift_x * cos(0-angle)) - (shift_y * sin(0-angle)));
-                   ptsy[i] = ((shift_x * sin(0-angle)) + (shift_y * cos(0-angle)));
+                   ptsx[i] = ((shift_x * cos(0-ref_yaw)) - (shift_y * sin(0-ref_yaw)));
+                   ptsy[i] = ((shift_x * sin(0-ref_yaw)) + (shift_y * cos(0-ref_yaw)));
                 }
                 //create spline
                 tk::spline s;
@@ -317,7 +315,7 @@ int main() {
                 //set (x,y) points to the spline
                 s.set_points(ptsx,ptsy);
 
-                for(int i = 0; i < path_size; i++)
+                for(int i = 0; i < previous_path_x.size(); i++)
                 {    
                   next_x_vals.push_back(previous_path_x[i]);
                   next_y_vals.push_back(previous_path_y[i]);
@@ -329,7 +327,7 @@ int main() {
 
                 double x_add_on = 0;
 
-                for(int i = 0; i < 50-path_size; i++)
+                for(int i = 1; i <= 50-previous_path_x.size(); i++)
                 {    
                   double N = (target_dist/(0.02*vel_ref/2.24));
                   double x_point = (x_add_on+(target_x/N));
@@ -341,11 +339,11 @@ int main() {
                   double y_ref = y_point;
 
                   //rotate back to normal coordinates
-                  x_point = (x_ref * cos(angle)-y_ref*sin(angle));
-                  y_point = (x_ref * sin(angle)+y_ref*cos(angle));
+                  x_point = (x_ref * cos(ref_yaw)-y_ref*sin(ref_yaw));
+                  y_point = (x_ref * sin(ref_yaw)+y_ref*cos(ref_yaw));
 
-                  x_point += x_ref;
-                  y_point += y_ref;
+                  x_point += ref_x;
+                  y_point += ref_y;
 
                   next_x_vals.push_back(x_point);
                   next_y_vals.push_back(y_point);
